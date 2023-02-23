322223955320655a5c2b6919528e7c9ee8da2365
XinyuTian1104 <michelle.tian.1104@gmail.com>
2023-02-23 03:52:08 -0500

    update test.py

--- test.py
+++ test.py
@@ -2,18 +2,14 @@ import os
 import time
 import requests
 
-CLUSTER_IP = "10.105.120.17"
-PORT = "30506"
-TEST_TIME = 300
-
 with open('test.log', 'w') as f:
     f.write('')
 
 
 def test_api():
     print("---- test started ----")
-    url = "http://{}:{}/api/recommend".format(CLUSTER_IP, PORT)
-    print('url: {}'.format(url))
+    url = "http://10.105.120.17:30506/api/recommend"
+    print('url: ', url)
 
     payload = {
         'songs': [
@@ -22,19 +18,17 @@ def test_api():
         ]
     }
 
-    for i in range(TEST_TIME):
+    for i in range(0, 100):
         try:
             response = requests.post(url, json=payload, timeout=0.5)
             response_json = response.json()
         except:
             response_json = "SERVICE_OFFLINE"
 
-        log = "TEST [{}/{}] TIME: {} {}".format(i + 1, TEST_TIME,
-                                                time.strftime("%Y-%m-%d %H:%M:%S"), response_json)
-        print(log)
+        print("[", i+1, " /100]", time.strftime("%Y-%m-%d %H:%M:%S"), response_json)
 
         with open('test.log', 'a') as f:
-            f.write(log + '\n')
+            f.write(time.strftime("%Y-%m-%d %H:%M:%S"), response_json, '\n')
         time.sleep(0.5)
 
 
