1d4a1241996f4cff791a6692a4f1b850773e90fd
XinyuTian1104 <michelle.tian.1104@gmail.com>
2023-02-23 00:57:51 -0500

    change data version

--- test.py
+++ test.py
@@ -9,21 +9,22 @@ TEST_TIME = 200
 
 def test_api():
     print("---- test started ----")
-    url = "http://{}:{}/api/recommend/".format(CLUSTER_IP, PORT)
+    url = "http://{}:{}/api/recommend".format(CLUSTER_IP, PORT)
+    print('url: {}'.format(url))
 
     payload = {
         'songs': [
-            '1985',
-            'Bohemian Rhapsody',
-            'I Want It That Way',
-            'Yesterday'
+            "1985",
+            "All The Small Things"
         ]
     }
 
     for i in range(TEST_TIME):
         response = requests.post(url, json=payload, timeout=1)
-        print("TEST [{}/{}]".format(i + 1, TEST_TIME), response.json())
+        print("TEST [{}/{}]".format(i + 1, TEST_TIME),
+              "TIME: {}".format(time.strftime("%Y-%m-%d %H:%M:%S")), response.json())
         time.sleep(1)
+
 
 if __name__ == "__main__":
     test_api()
