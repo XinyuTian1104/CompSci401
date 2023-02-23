df446a9666f29993122f09b4461780c9f872d8b6
XinyuTian1104 <michelle.tian.1104@gmail.com>
2023-02-23 01:22:11 -0500

    test script

--- test.py
+++ test.py
@@ -4,7 +4,10 @@ import requests
 
 CLUSTER_IP = "10.105.120.17"
 PORT = "30506"
-TEST_TIME = 200
+TEST_TIME = 300
+
+with open('test.log', 'w') as f:
+    f.write('')
 
 
 def test_api():
@@ -20,10 +23,19 @@ def test_api():
     }
 
     for i in range(TEST_TIME):
-        response = requests.post(url, json=payload, timeout=1)
-        print("TEST [{}/{}]".format(i + 1, TEST_TIME),
-              "TIME: {}".format(time.strftime("%Y-%m-%d %H:%M:%S")), response.json())
-        time.sleep(1)
+        try:
+            response = requests.post(url, json=payload, timeout=0.5)
+            response_json = response.json()
+        except:
+            response_json = "SERVICE_OFFLINE"
+
+        log = "TEST [{}/{}] TIME: {} {}".format(i + 1, TEST_TIME,
+                                                time.strftime("%Y-%m-%d %H:%M:%S"), response_json)
+        print(log)
+
+        with open('test.log', 'a') as f:
+            f.write(log + '\n')
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
